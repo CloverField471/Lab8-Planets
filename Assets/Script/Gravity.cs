c6e3c4492c667f8faf62796d8af1927fb140d2f7
KAKanna <mlg666pop@gmail.com>
2025-03-10 16:29:18 +0700

    finish

--- Assets/Script/Gravity.cs
+++ Assets/Script/Gravity.cs
@@ -1,19 +1,47 @@
+using NUnit.Framework;
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Gravity : MonoBehaviour
 {
     Rigidbody rb;
+    const float G = 0.006674f;
+
+    public static List<Gravity> planetList;
 
     private void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        if(planetList == null)
+        {
+            planetList = new List<Gravity>();
+        }
+
+        planetList.Add(this);
     }
 
+    private void FixedUpdate()
+    {
+        foreach(var planet in planetList)
+        {
+            if(planet != this)
+            {
+                Attract(planet);
+            }
+        }
+    }
     void Attract(Gravity other)
     {
         Rigidbody otherRb = other.rb;
 
         Vector3 direction = rb.position - otherRb.position;
+        //Get distance in meter
+        float distance = direction.magnitude;
 
+        //Calculate Gravity Force 
+        float forceMagnitude = G * ((rb.mass * otherRb.mass) / Mathf.Pow(distance, 2));
+        Vector3 finalForce = forceMagnitude * direction.normalized;
+
+        otherRb.AddForce(finalForce);
     }
 }
